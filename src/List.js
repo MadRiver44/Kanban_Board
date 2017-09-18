0441935ae77850842547cf43a3f0385eab19c700
Kevin Turney <super996@optonline.net>
2017-09-18 10:33:32 -0400

    add key and color prop

--- src/List.js
+++ src/List.js
@@ -4,9 +4,11 @@ import Card from './Card.js';
 class List extends Component {
   render() {
     let cards = this.props.cards.map((card) => {
-      return <Card id={card.id}
+      return <Card key={card.id}
+                   id={card.id}
                    title={card.title}
                    description={card.description}
+                   color={card.color}
                    tasks={card.tasks} />
     });
     return (
