65304576df6a57707e21303e9139e922297d5076
Kevin Turney <super996@optonline.net>
2017-09-19 09:57:59 -0400

    receive taskCallbacks as props, create onChange handler, add, delete, toggle

--- src/CheckList.js
+++ src/CheckList.js
@@ -1,29 +1,58 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class CheckList extends Component {
-  render() {
-    let tasks = this.props.tasks.map((task) => (
-      <li key={task.id} className="checklist_task">
-        <input type="checkbox" defaultChecked={task.done} />
-        {task.name}
-        <a href="#" className="checklist_task--remove"></a>
-      </li>
-      ));
-    return (
-        <div className="checklist">
-          <ul>{tasks}</ul>
-        {/* uncontrolled component*/}
-          <input type="text"
-                 className="checkList--add-task"
-                 placeholder="Type, then hit enter to add a task"/>
-        </div>
-      );
+  checkInputKeyPress(event) {
+    if (event.key === 'Enter') {
+      this.props.taskCallbacks.add(this.props.cardId, event.target.value)
+      event.target.value = ''
     }
   }
+  render() {
+    let tasks = this.props.tasks.map((task, taskIndex) => (
+      <li key={task.id} className="checklist_task">
+        <input
+          type="checkbox"
+          checked={task.done}
+          onChange={this.props.taskCallbacks.toggle.bind(
+            null,
+            this.props.cardId,
+            task.id,
+            taskIndex,
+          )}
+        />
+        {task.name}{' '}
+        <a
+          href="#"
+          className="checklist_task--remove"
+          onClick={this.props.taskCallbacks.delete.bind(
+            null,
+            this.props.cardId,
+            task.id,
+            taskIndex,
+          )}
+        />
+      </li>
+    ))
+    return (
+      <div className="checklist">
+        <ul>{tasks}</ul>
+        {/* uncontrolled component*/}
+        <input
+          type="text"
+          className="checkList--add-task"
+          placeholder="Type, then hit enter to add a task"
+          onKeyPress={this.checkInputKeyPress.bind(this)}
+        />
+      </div>
+    )
+  }
+}
 
 CheckList.propTypes = {
   cardId: PropTypes.number,
-  tasks: PropTypes.arrayOf(PropTypes.object)
-};
+  tasks: PropTypes.array,
+  taskCallbacks: PropTypes.object,
+}
 
-export default CheckList;
+export default CheckList
