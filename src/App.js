510a98d363131e51b2162adcc2e50e494c812c0f
Kevin Turney <super996@optonline.net>
2017-09-19 09:59:16 -0400

    remove hard coded cards, import KanbanAppContainer.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
-import React from 'react';
-import { render } from 'react-dom';
-import KanbanBoard from './KanbanBoard.js';
-
+import React from 'react'
+import { render } from 'react-dom'
+import KanbanAppContainer from './KanbanAppContainer.js'
 
 // hard coded data model
+/*
 let cardsList = [
-{
-  id: 1,
-  title: "Read the manual!!",
-  description: "I should read **everything**.",
-  color: '#BD8D31',
-  status: "in-progress",
-  tasks: []
-},
-{
-  id: 2,
-  title: "Write some code.",
-  description: "practice, practice, practice!",
-  color: '#3A7E28',
-  status: "todo",
-  tasks: [
-    {
-      id: 1,
-      name: "create more react projects.",
-      done: true,
-    },
-    {
-      id: 2,
-      name: "kanban Board",
-      done: false
-    },
-    {id: 3,
-     name: "My education",
-     done: false
-   }
-   ]
-  }
-];
+  {
+    id: 1,
+    title: 'Read the manual!!',
+    description: 'I should read **everything**.',
+    color: '#BD8D31',
+    status: 'in-progress',
+    tasks: [],
+  },
+  {
+    id: 2,
+    title: 'Write some code.',
+    description: 'practice, practice, practice!',
+    color: '#3A7E28',
+    status: 'todo',
+    tasks: [
+      {
+        id: 1,
+        name: 'create more react projects.',
+        done: true,
+      },
+      {
+        id: 2,
+        name: 'kanban Board',
+        done: false,
+      },
+      {
+        id: 3,
+        name: 'My education',
+        done: false,
+      },
+    ],
+  },
+]
+*/
 
-render(<KanbanBoard cards={cardsList} />, document.getElementById('root'));
+render(<KanbanAppContainer />, document.getElementById('root'))
