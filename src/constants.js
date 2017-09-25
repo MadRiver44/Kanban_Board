ce09107e70fd307ff3497d0ea439d5e552f5f5d4
Kevin Turney <super996@optonline.net>
2017-09-25 10:55:39 -0400

    add an identifier readable across modules

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+/* dragSource and dropTarget require a type parameter used to uniquely
+  identify the draggable component. Rather than use a string, we can use a constant
+  as an identifier that can be referenced across all files
+  */
+
+export default {
+  CARD: 'card',
+};
