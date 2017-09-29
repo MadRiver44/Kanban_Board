43ed706ff07929ffc3682e6f0c95db9a8106da33
Kevin Turney <super996@optonline.net>
2017-09-29 16:20:15 -0400

    create a card modal

--- src/KanbanBoard.js
+++ src/KanbanBoard.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react';
-import { DragDropContext } from 'react-dnd';
+import React, {Component} from 'react';
+import {DragDropContext} from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import PropTypes from 'prop-types';
 import List from './List.js';
+import {Link} from 'react-router';
 
 class KanbanBoard extends Component {
   render() {
+    let cardModal =
+      this.props.children &&
+      React.cloneElement(this.props.children, {
+        cards: this.props.cards,
+        cardCallbacks: this.props.cardCallbacks,
+      });
+
     return (
       <div className="app">
+        <Link to="/new" className="float-button">
+          +
+        </Link>
         <List
           id="todo"
           title="To Do"
@@ -29,6 +40,7 @@ class KanbanBoard extends Component {
           cardCallbacks={this.props.cardCallbacks}
           cards={this.props.cards.filter(card => card.status === 'done')}
         />
+        {cardModal}
       </div>
     );
   }
