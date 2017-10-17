b00d2d40634024790c2ba958bbe08794c9d61c65
Kevin Turney <super996@optonline.net>
2017-10-17 15:09:34 -0400

    ...

--- webpack.config.js
+++ webpack.config.js
@@ -37,9 +37,9 @@ if (process.env.NODE_ENV === 'production') {
   config.devtool = false;
   config.plugins = [
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({comments: false}),
+    new webpack.optimize.UglifyJsPlugin({ comments: false }),
     new webpack.DefinePlugin({
-      'process.env': {NODE_ENV: JSON.stringify('production')},
+      'process.env': { NODE_ENV: JSON.stringify('production') },
     }),
   ];
 }
