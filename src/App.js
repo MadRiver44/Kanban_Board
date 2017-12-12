8e0decc0f2cf2c9b61d040b8944b6d7345b486a1
Kevin Turney <super996@optonline.net>
2017-12-11 19:33:57 -0500

    remove .procfile

--- src/App.js
+++ src/App.js
@@ -8,43 +8,6 @@ import KanbanBoard from './KanbanBoard.js';
 import EditCard from './EditCard.js';
 import NewCard from './NewCard.js';
 
-// hard coded data model
-/*
-let cardsList = [
-  {
-    id: 1,
-    title: 'Read the manual!!',
-    description: 'I should read **everything**.',
-    color: '#BD8D31',
-    status: 'in-progress',
-    tasks: [],
-  },
-  {
-    id: 2,
-    title: 'Write some code.',
-    description: 'practice, practice, practice!',
-    color: '#3A7E28',
-    status: 'todo',
-    tasks: [
-      {
-        id: 1,
-        name: 'create more react projects.',
-        done: true,
-      },
-      {
-        id: 2,
-        name: 'kanban Board',
-        done: false,
-      },
-      {
-        id: 3,
-        name: 'My education',
-        done: false,
-      },
-    ],
-  },
-]
-*/
 render(
   <Router history={createBrowserHistory()}>
     <Route component={KanbanAppContainer}>
