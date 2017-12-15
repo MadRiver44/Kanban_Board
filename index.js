8b6df5bb38c55c4f2a2f8f486154f699cac43ade
Kevin Turney <super996@optonline.net>
2017-12-15 14:47:00 -0500

    bundle path

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { render } from 'react-dom';
 import { Router, Route } from 'react-router';
 //import { Provider } from 'react-redux';
